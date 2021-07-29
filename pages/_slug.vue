4ed36ce023706d5a3ccc5f1d6e05fc1b6e7fec0f
Nick Basil <thebasil88@gmail.com>
2021-07-29 09:25:16 -0400

    modular changes

---
+++ pages/_slug.vue
@@ -0,0 +1,29 @@
+<template>
+	<div>
+		<div v-for="t in document.team">
+			<Hero v-if="t.type == 'hero,'" :title="t.title"/>
+			<h1 v-if="t.type == 'prices,'">Prices</h1>
+			<h1 v-if="t.type == 'fake,'">Prices</h1>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+ async asyncData({ $content, params  }) {
+    const document = await $content("pages", params.slug).fetch();
+	
+    return { document };
+ },
+ methods: {
+	showTeam: function(){
+		console.log(this.document.team, "team");
+	},
+ },
+ head() {
+    return {
+      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
+    };
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,10 @@
 	<div>
 		<h1 class="text-indigo-600">HI!</h1>
 		<div v-for="document in documents" key="document.slug" class="prose w-full max-w-full">
-			<NuxtLink :to="document.path">{{ document.title }}</NuxtLink>
+			<NuxtLink :to="document.slug">{{ document.title }}</NuxtLink>
+			<div v-for="t in document.team">
+				
+			</div>
 		</div>
 	</div>
 </template>
@@ -10,8 +13,8 @@
 <script>
 export default {
  async asyncData({ $content }) {
-    const documents = await $content("blog").fetch();
-	console.log(documents);
+    const documents = await $content("pages").fetch();
+	console.log(documents.team);
     return { documents };
  },
  head() {
